c71b6d16cd2c3d70085238f45682e82b16801b22
赵晗旭 <1948287545@>
2018-06-27 08:48:42 +0800

    更改测试服务器

--- widget/script/config.js
+++ widget/script/config.js
@@ -1,5 +1,5 @@
 const cfg = {
-  baseUrl: "http://47.104.103.114:8803/",//生产环境
+  baseUrl: "http://47.104.103.114:8801/",//生产环境
   pageSize: 20,
   userCacheKEY: "userCache20171220",
   dictionaryCacheKEY: "dictionaryCache20180103"
